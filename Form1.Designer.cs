fa9f54cb6cf17290f82e1337f95328e06d687870
Azra <yukselazrael@gmail.com>
2024-01-27 23:41:38 +0300

    Added Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace FirstWinFormsAppExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.UserNameTxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.PasswordTxt = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.login = new System.Windows.Forms.Button();
+            this.newButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.LightCoral;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn1.Location = new System.Drawing.Point(539, 558);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(183, 37);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Don\'t Touch!";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(38, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hi, Innocent!";
+            // 
+            // UserNameTxt
+            // 
+            this.UserNameTxt.Location = new System.Drawing.Point(37, 115);
+            this.UserNameTxt.Name = "UserNameTxt";
+            this.UserNameTxt.Size = new System.Drawing.Size(119, 22);
+            this.UserNameTxt.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(162, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Write Username!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(162, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Write Password!";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // PasswordTxt
+            // 
+            this.PasswordTxt.Location = new System.Drawing.Point(37, 151);
+            this.PasswordTxt.Name = "PasswordTxt";
+            this.PasswordTxt.PasswordChar = '☺';
+            this.PasswordTxt.Size = new System.Drawing.Size(119, 22);
+            this.PasswordTxt.TabIndex = 4;
+            // 
+            // login
+            // 
+            this.login.BackColor = System.Drawing.SystemColors.Info;
+            this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.login.Location = new System.Drawing.Point(66, 183);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(196, 37);
+            this.login.TabIndex = 6;
+            this.login.Text = "Wellcome to Hell";
+            this.login.UseVisualStyleBackColor = false;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(131, 430);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(84, 30);
+            this.newButton.TabIndex = 7;
+            this.newButton.Text = "Open";
+            this.newButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1288, 607);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.PasswordTxt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.UserNameTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Azra";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox UserNameTxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox PasswordTxt;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Button newButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWinFormsAppExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wellcome to Abyss..");
+            label1.Text = "Your Future is Coming...";
+            label1.ForeColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textValue = UserNameTxt.Text;
+            MessageBox.Show(textValue);
+            UserNameTxt.Clear();
+            //butona tıkladıktan sonra yeniden text boxa bir şey yazmak için focus kullan.
+            UserNameTxt.Focus();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ekran her açıldığında focusu çalıştırmak için
+            UserNameTxt.Focus();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            string username = "Human";
+            string password = "1453";
+
+            if(username == UserNameTxt.Text && password == PasswordTxt.Text)
+            {
+                MessageBox.Show("Hi, innocent " + username,
+                    "you signed ur death",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                createElement();
+            }
+            else
+            {
+                MessageBox.Show("Dont try to escape",
+                        "Try again",
+                        MessageBoxButtons.RetryCancel,
+                        MessageBoxIcon.Error);
+            }
+        }
+
+
+        public void createElement()
+        {
+            Label label = new Label();
+            Button button = new Button();
+
+            label.Name = "NewLabel";
+            label.Text = "Game Starting...";
+            label.ForeColor = Color.Red;
+            label.Location = new Point(86, 200);
+
+            this.Controls.Add(label);
+
+            newButton = new Button();
+            newButton.Name = "NewButton";
+            newButton.Text =   "Open";
+            newButton.Location = new Point(100, 200);
+            newButton.Click += NewButton_Click;
+
+            this.Controls.Add(newButton);
+
+
+            UserNameTxt.Visible = false;
+            PasswordTxt.Visible = false;
+            login.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+
+
+        }
+
+
+
+    }
+}
